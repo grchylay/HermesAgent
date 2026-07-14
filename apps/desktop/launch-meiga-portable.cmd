289eb527c109d3e00dccb194a6fe44c07f55eaca
laygraciana <gracianalay39@gmail.com>
2026-07-14 11:18:01 +0800

    release: prepare portable package v0.1.1

---
+++ apps/desktop/launch-meiga-portable.cmd
@@ -0,0 +1,5 @@
+@echo off
+set HERMES_DESKTOP_HERMES_ROOT=D:\tmp\RuyiHermesAgent
+set HERMES_HOME=D:\tmp\RuyiHermesAgent\workspace
+echo Launching Meiga Agent (portable)...
+D:\tmp\RuyiHermesAgent\apps\desktop\release\win-unpacked\Meiga.exe
